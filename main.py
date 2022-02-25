9f9026456bb7876497805169026929707e6a4939
JoshuaBog09 <95229035+JoshuaBog09@users.noreply.github.com>
2022-02-25 12:27:39 +0100

    file reader

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import glob
+
+
+def load_file(file: str, skip_rows: int = None):
+    return pd.read_csv(file, sep="\t", skiprows=skip_rows)
+
+
+def files_in_directory(folder: str, file_extension: str) -> list:
+    return glob.glob(f"Data/{folder}/*.{file_extension}")
 
 
 def main():
     """
     Main
     """
-    print("Hell world")
+    files_dap = files_in_directory("DispAndForce", "txt")
+    files_es = files_in_directory("Experimental strains", "txt")
+    files_fem = files_in_directory("FEM", "out")
+    files_md = files_in_directory("ModelData", "txt")
+
+    data = load_file("Data/DispAndForce/CurentVoltageReading3.txt")
+    data1 = load_file("Data/Experimental strains/Measurements2014_05_22.txt", 4)
+
+    print(f"{files_dap=}")
+    print(f"{files_es=}")
+    print(f"{files_fem=}")
+    print(f"{files_md=}")
+
+    print(f"{data=}")
+    print(f"{data1=}")
 
 
 if __name__ == "__main__":
