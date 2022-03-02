590333435176073a9844308bfeea8b932e781240
JoshuaBog09 <95229035+JoshuaBog09@users.noreply.github.com>
2022-03-02 12:20:16 +0100

    reader and md files

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ import glob
 
 
 def load_file(file: str, skip_rows: int = None):
-    return pd.read_csv(file, sep="\t", skiprows=skip_rows)
+    data_file = pd.read_csv(file, sep="\t", skiprows=skip_rows)
+    headers = list(data_file.columns.values)
+    for index, header in enumerate(headers):
+        if "-----" in header:
+            data_file.rename(columns={header: 'Timestamp[t]'}, errors='raise', inplace=True)
+    return data_file
 
 
 def files_in_directory(folder: str, file_extension: str) -> list:
