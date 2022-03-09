dea25cd2c0a33f5d6c6f44b948c4d4226e58c414
JoshuaBog09 <95229035+JoshuaBog09@users.noreply.github.com>
2022-03-09 10:34:08 +0100

    removed plotter

--- main.py
+++ main.py
@@ -34,54 +34,6 @@ def perftimer(func):
     return wrapper
 
 
-class Plotter:
-
-    def __init__(self, x, y, x_label, y_label, legend_labels=None) -> None:
-        if any(isinstance(i, list) for i in x):
-            self.x = x
-            self.y = y
-        if any(isinstance(i, pd.DataFrame) for i in x):
-            self.x = x
-            self.y = y
-        if any(isinstance(i, pd.Series) for i in x):
-            self.x = x
-            self.y = y
-        else:
-            self.x = [x]
-            self.y = [y]
-        self.x_label = str(x_label)
-        self.y_label = str(y_label)
-        if legend_labels:
-            self.legend = legend_labels
-        else:
-            self.legend = [None] * 2
-
-    def plotgraphs(self):
-        for i, j, k in zip(self.x, self.y, self.legend):
-            print("loop")
-            if len(i) != len(j):
-                raise Exception(f"Can't create graph, with irregular points ({len(i)}x{len(j)})")
-            if k:
-                plt.plot(i, j, label=self.legend)
-            else:
-                plt.plot(i, j)
-        plt.ylabel(self.y_label)
-        plt.xlabel(self.x_label)
-        if None not in self.legend:
-            plt.legend()
-        plt.show()
-
-    @classmethod
-    def commaformat(cls,):
-        pass
-
-    def __str__(self):
-        pass
-
-    def __repr__(self):
-        pass
-
-
 @perftimer
 def main():
     """
@@ -105,17 +57,6 @@ def main():
 
     file = pd.read_csv("Data/ModelData/target_shape.csv")
     file2 = pd.read_csv("Data/ModelData/target_shape.csv")
-    a = [file["x[mm]"], file2["y[mm]"]]
-    b = [file["y[mm]"], file2["x[mm]"]]
-    a = [1,2,3]
-    b = [1,2,3]
-
-    plot = Plotter(a,b,"x","y")
-    plot.plotgraphs()
-    print(plot.__dict__)
-    # print(plot.x[0])
-    # print("----")
-    # print(plot.x[1])
 
 
 if __name__ == "__main__":
