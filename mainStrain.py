d397b3ee8af8d5916d8440203c6caff69144f2bc
Mick929 <mick.resink@gmail.com>
2022-03-16 10:36:32 +0100

    update
    update

--- mainStrain.py
+++ mainStrain.py
@@ -27,6 +27,52 @@ print(data2)
 
 
 
+
 #expData = np.genfromtxt("Data/Experimental Strains/Measurements2014_05_22.csv", delimiter=',')
 expData = pd.DataFrame(load_file("Data/Experimental Strains/Measurements2014_05_22.csv")).to_numpy
 print(expData)
+
+
+'''
+v v make graphs v v
+'''
+# symmetry (bending analysis), FEm vs experimental, time vs asymetry (optional)
+fig, ax = plt.subplots(2,2)
+
+# symmetry analyis plot FEM
+ax[0,0].plot(x1, y1, linewidth=2.0, label = 'sin', color = 'orange')
+
+ax[0,0].set(xlim=(0, 8), xticks=np.arange(1, 8),
+       ylim=(0, 8), yticks=np.arange(1, 8))
+
+ax[0,0].set_title('Bending Analyis FEM')
+
+# symmetry analyis plot FEM
+ax[0,1].plot(x1, y1, linewidth=2.0, label = 'sin', color = 'orange')
+
+ax[0,1].set(xlim=(0, 8), xticks=np.arange(1, 8),
+       ylim=(0, 8), yticks=np.arange(1, 8))
+
+ax[0,1].set_title('Bending Analyis FEM')
+
+
+
+# FEM vs experimental data
+ax[1,0].plot(x1, y1, linewidth=2.0, label = 'sin', color = 'orange')
+ax[1,0].plot(x1, y2, linewidth=2.0, label = 'cos',  color = 'blue' ) 
+
+ax[1,0].set(xlim=(0, 8), xticks=np.arange(1, 8),
+       ylim=(0, 8), yticks=np.arange(1, 8))
+
+ax[1,0].legend(['sin','cos'])
+
+
+#asymmetry over time (optional)
+ax[1,1].plot(x1, y2, linewidth=2.0, label = 'cos', color = 'blue')
+
+
+ax[1,1].set(xlim=(0, 8), xticks=np.arange(1, 8),
+       ylim=(0, 8), yticks=np.arange(1, 8))
+
+
+plt.show()
