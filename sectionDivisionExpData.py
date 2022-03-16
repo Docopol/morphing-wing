04e869aa9bb662ff6ac03684f47d306c522586ed
TomasQReis <tomas.qreis@gmail.com>
2022-03-16 12:32:09 +0100

    Section Division

--- sectionDivisionExpData.py
+++ sectionDivisionExpData.py
@@ -1,3 +1,5 @@
+from contextlib import redirect_stderr
+from sqlite3 import Timestamp
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -12,18 +14,14 @@ def load_file(file: str, skip_rows: int = None):
     data_file = pd.read_csv(file, sep="\t", skiprows=skip_rows, header=None)
     return data_file
 
-'''
-data2 = load_file("Data/FEM/shell_loadstep3_str.out")
-data2 = pd.DataFrame(data2).to_numpy()
 
-print(data2)
-'''
+# This stuff will go into the final code of this section.
 
-#expData = np.genfromtxt("Data/Experimental Strains/Measurements2014_05_22.csv", delimiter=',')
 experimental_strain_rows = pd.DataFrame(load_file("Data/Experimental Strains/Measurements2014_05_22.csv")).to_numpy()
-print(experimental_strain_rows.shape)
 rows, columns = experimental_strain_rows.shape
 experimental_strain = np.array(np.zeros((rows,4263)),dtype=str)
+
+
 
 for i in range(rows):
     line = np.zeros((1,4263))
@@ -33,16 +31,15 @@ for i in range(rows):
     #print(line.shape)
     experimental_strain[i] = line
 
-print(experimental_strain.shape)
 
 fiberLengths = [float(i) for i in experimental_strain[0,1:]] # This should not be changed
-strainMeasures = [float(i) for i in experimental_strain[25,1:]] # This will change based on which timestep is wanted
 
+'''
+# To access specific strains at timestamp change the X value in:
+# float(i) for i in experimental_strain[X,1:]
+strainMeasures = [float(i) for i in experimental_strain[20,1:]] # This will change based on which timestep is wanted
+''' # This shit should no longer be needed, just kept in case.
 
-
-#plt.style.use('_mpl-gallery')
-plt.plot(fiberLengths,strainMeasures,linewidth=0.5)
-plt.show()
 
 '''
 WARNING: 
@@ -69,26 +66,104 @@ def lengthSectionSplitter(fiberLengths):
     return sections
 
 '''
-addStrainTimestamp_X:
+SectionX:
 
-Takes a timestamp (In seconds from start of test), a list of strain measures and a list of fiber lengths.
-Adds a new row to a pre-existing sectionC numpy array of timestamps and strains of sectionX.
+Takes a timestamp (Starting from 1, 0 is the lengths), the list of fiber lists and array of experimental strains.
+returns the corresponding section lengths and strains, both as measured on the fiber cable.
 
-WARNING: Since this adds the new timestamp at the bootom, 
-it assumes that this will be done in the proper order before the function is called.
-'''
+Note that all the segments have different sizes, must first be fitted before matching together.
+
+WARNING: Here the lengths are still uncalibrated, they need to be stretched/squeezed in order to fit the actual contour of the airfoil.
+WARNING: The strain data for Sections C and D are given backwards, as in, they start at the "end" of the contour and begin 
+'''# I know its redundant but fuck off.
 
 #print(fiberLengths)
-def splitSections_C(timeStamp ,fiberLengths, strainMeasures, sectionC):
+def sectionC(timeStamp ,fiberLengths, strainMeasures):
     for i in range(len(fiberLengths)):
-        '''if abs(fiberLengths[i] - 0.190102)<10**(-3):
-            sectionC = np.zeros((80,1))
-            sectionC[,0]=testStamps
-            sectionC[]
+        if abs(fiberLengths[i] - 0.190102)<10**(-3):
+            firstTerm = i+1
+        if fiberLengths[i] == 1.25324:
+            lastTerm = i+2 
+            break
+    sectionCStrains = [float(a) for a in strainMeasures[timeStamp, firstTerm:lastTerm]]
+    sectionCLengths = [float(a) for a in fiberLengths[firstTerm:lastTerm]]
+    return sectionCStrains, sectionCLengths
 
-testStamps = np.ones((80,1))
-splitSections_C(testStamps, fiberLengths,strainMeasures)
+def sectionA(timeStamp ,fiberLengths, strainMeasures):
+    for i in range(len(fiberLengths)):
+        if fiberLengths[i] == 1.58908:
+            firstTerm = i+1
+        if fiberLengths[i] == 2.59791:
+            lastTerm = i+2 
+            break
+    sectionAStrains = [float(a) for a in strainMeasures[timeStamp, firstTerm:lastTerm]]
+    sectionALengths = [float(a) for a in fiberLengths[firstTerm:lastTerm]]
+    return sectionAStrains, sectionALengths
+
+def sectionB(timeStamp ,fiberLengths, strainMeasures):
+    for i in range(len(fiberLengths)):
+        if fiberLengths[i] == 2.75864:
+            firstTerm = i+1
+        if fiberLengths[i] == 3.77008:
+            lastTerm = i+2 
+            break
+    sectionAStrains = [float(a) for a in strainMeasures[timeStamp, firstTerm:lastTerm]]
+    sectionALengths = [float(a) for a in fiberLengths[firstTerm:lastTerm]]
+    return sectionAStrains, sectionALengths
+
+def sectionD(timeStamp ,fiberLengths, strainMeasures):
+    for i in range(len(fiberLengths)):
+        if fiberLengths[i] == 4.07065:
+            firstTerm = i+1
+        if fiberLengths[i] == 5.17115:
+            lastTerm = i+2 
+            break
+    sectionAStrains = [float(a) for a in strainMeasures[timeStamp, firstTerm:lastTerm]]
+    sectionALengths = [float(a) for a in fiberLengths[firstTerm:lastTerm]]
+    return sectionAStrains, sectionALengths
+
+
 '''
+fitSection.
+Takes the sections lengths, and their appropriate callibration start and end points.
+Returns a set of intervals of the same size as the section lengths which has been squeezed to fit the actual contour.
+'''
+
+def fitSection(sectionLengths, callibrationStart, callibrationEnd):
+    totalLength = callibrationEnd - callibrationStart
+    gapNumber = len(sectionLengths) - 1
+    gapLength = totalLength/gapNumber
+
+    fittedLength = np.zeros(gapNumber+1)
+    fittedLength[0]=callibrationStart
+
+    for i in range(gapNumber):
+        fittedLength[i+1]=fittedLength[i]+gapLength
+
+    return fittedLength
+
+
+AStrains, ALengths = sectionA(20, fiberLengths, experimental_strain)
+CStrains, CLengths = sectionC(20, fiberLengths, experimental_strain)
+CStrains = [-x for x in CStrains]
+
+BStrains, BLengths = sectionB(20, fiberLengths, experimental_strain)
+DStrains, DLengths = sectionD(20, fiberLengths, experimental_strain)
+DStrains = [-x for x in DStrains]
+
+CLengths = fitSection(CLengths, 0.07, 1.027)
+ALengths = fitSection(ALengths, 0.05, 1.05)
+
+ACSections = [ALengths, AStrains, CLengths, CStrains]
+BDSections = []
+
+#print(CStrain)
+ 
+plt.plot(ACSections[2], ACSections[3], linewidth=0.5) # Inside Graph
+plt.plot(ACSections[0], ACSections[1], linewidth=0.5, color='r') # Outside Graph
+plt.show()
+
+
 '''
 FiberLocation	ActualLocation
 0.190102	0.07
@@ -100,4 +175,4 @@ FiberLocation	ActualLocation
 0.984042	0.759
 1.25324		1.027
 '''
-print(fiberLengths)
+#print(fiberLengths)
