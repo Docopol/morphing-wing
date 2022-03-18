ab72527ec459be723269341cde034e5032713064
Roel <r.vanleeuwen-4@student.tudelft.nl>
2022-03-18 12:08:37 +0100

    update mainStrain

--- mainStrain.py
+++ mainStrain.py
@@ -73,46 +73,46 @@ v v make graphs v v
 
 # strain data 1
 plt.subplot(2, 2, 1)
-plt.plot(femNodeLocation[1 - 1], femStrainData1s[1 - 1][2:, 4], label="loadstep 1.1")
-plt.plot(femNodeLocation[2 - 1], femStrainData1s[2 - 1][2:, 4], label="loadstep 2.1")
-plt.plot(femNodeLocation[3 - 1], femStrainData1s[3 - 1][2:, 4], label="loadstep 3.1")
-plt.plot(femNodeLocation[4 - 1], femStrainData1s[4 - 1][2:, 4], label="loadstep 4.1")
-plt.plot(femNodeLocation[5 - 1], femStrainData1s[5 - 1][2:, 4], label="loadstep 5.1")
+plt.plot(femNodeLocation[1 - 1], femStrainData1s[1 - 1][:, 4], label="loadstep 1.1")
+plt.plot(femNodeLocation[2 - 1], femStrainData1s[2 - 1][:, 4], label="loadstep 2.1")
+plt.plot(femNodeLocation[3 - 1], femStrainData1s[3 - 1][:, 4], label="loadstep 3.1")
+plt.plot(femNodeLocation[4 - 1], femStrainData1s[4 - 1][:, 4], label="loadstep 4.1")
+plt.plot(femNodeLocation[5 - 1], femStrainData1s[5 - 1][:, 4], label="loadstep 5.1")
 plt.legend()
 
 # strain data 2
 plt.subplot(2, 2, 2)
-plt.plot(femNodeLocation[1 - 1], femStrainData2s[1 - 1][2:, 4], label="loadstep 1.2")
-plt.plot(femNodeLocation[2 - 1], femStrainData2s[2 - 1][2:, 4], label="loadstep 2.2")
-plt.plot(femNodeLocation[3 - 1], femStrainData2s[3 - 1][2:, 4], label="loadstep 3.2")
-plt.plot(femNodeLocation[4 - 1], femStrainData2s[4 - 1][2:, 4], label="loadstep 4.2")
-plt.plot(femNodeLocation[5 - 1], femStrainData2s[5 - 1][2:, 4], label="loadstep 5.2")
+plt.plot(femNodeLocation[1 - 1], femStrainData2s[1 - 1][:, 4], label="loadstep 1.2")
+plt.plot(femNodeLocation[2 - 1], femStrainData2s[2 - 1][:, 4], label="loadstep 2.2")
+plt.plot(femNodeLocation[3 - 1], femStrainData2s[3 - 1][:, 4], label="loadstep 3.2")
+plt.plot(femNodeLocation[4 - 1], femStrainData2s[4 - 1][:, 4], label="loadstep 4.2")
+plt.plot(femNodeLocation[5 - 1], femStrainData2s[5 - 1][:, 4], label="loadstep 5.2")
 plt.legend()
 
 # strain data difference of data 1 and 2
 plt.subplot(2, 2, 3)
-plt.plot(femNodeLocation[1 - 1], abs(abs(femStrainData1s[1 - 1][2:, 4]) - abs(femStrainData2s[1 - 1][2:, 4])),
+plt.plot(femNodeLocation[1 - 1], abs(abs(femStrainData1s[1 - 1][:, 4]) - abs(femStrainData2s[1 - 1][:, 4])),
          label="loadstep 1")
-plt.plot(femNodeLocation[2 - 1], abs(abs(femStrainData1s[2 - 1][2:, 4]) - abs(femStrainData2s[2 - 1][2:, 4])), label="loadstep 2")
-plt.plot(femNodeLocation[3 - 1], abs(abs(femStrainData1s[3 - 1][2:, 4]) - abs(femStrainData2s[3 - 1][2:, 4])), label="loadstep 3")
-plt.plot(femNodeLocation[4 - 1], abs(abs(femStrainData1s[4 - 1][2:, 4]) - abs(femStrainData2s[4 - 1][2:, 4])), label="loadstep 4")
-plt.plot(femNodeLocation[5 - 1], abs(abs(femStrainData1s[5 - 1][2:, 4]) - abs(femStrainData2s[5 - 1][2:, 4])), label="loadstep 5")
+plt.plot(femNodeLocation[2 - 1], abs(abs(femStrainData1s[2 - 1][:, 4]) - abs(femStrainData2s[2 - 1][:, 4])), label="loadstep 2")
+plt.plot(femNodeLocation[3 - 1], abs(abs(femStrainData1s[3 - 1][:, 4]) - abs(femStrainData2s[3 - 1][:, 4])), label="loadstep 3")
+plt.plot(femNodeLocation[4 - 1], abs(abs(femStrainData1s[4 - 1][:, 4]) - abs(femStrainData2s[4 - 1][:, 4])), label="loadstep 4")
+plt.plot(femNodeLocation[5 - 1], abs(abs(femStrainData1s[5 - 1][:, 4]) - abs(femStrainData2s[5 - 1][:, 4])), label="loadstep 5")
 plt.legend()
 plt.show()
 
-# FEM vs experimental data
-ax[1, 0].plot(x1, y1, linewidth=2.0, label='sin', color='orange')
-ax[1, 0].plot(x1, y2, linewidth=2.0, label='cos', color='blue')
-
-ax[1, 0].set(xlim=(0, 8), xticks=np.arange(1, 8),
-             ylim=(0, 8), yticks=np.arange(1, 8))
-
-ax[1, 0].legend(['sin', 'cos'])
-
-# asymmetry over time (optional)
-ax[1, 1].plot(x1, y2, linewidth=2.0, label='cos', color='blue')
-
-ax[1, 1].set(xlim=(0, 8), xticks=np.arange(1, 8),
-             ylim=(0, 8), yticks=np.arange(1, 8))
-
-plt.show()
+# # FEM vs experimental data
+# ax[1, 0].plot(x1, y1, linewidth=2.0, label='sin', color='orange')
+# ax[1, 0].plot(x1, y2, linewidth=2.0, label='cos', color='blue')
+#
+# ax[1, 0].set(xlim=(0, 8), xticks=np.arange(1, 8),
+#              ylim=(0, 8), yticks=np.arange(1, 8))
+#
+# ax[1, 0].legend(['sin', 'cos'])
+#
+# # asymmetry over time (optional)
+# ax[1, 1].plot(x1, y2, linewidth=2.0, label='cos', color='blue')
+#
+# ax[1, 1].set(xlim=(0, 8), xticks=np.arange(1, 8),
+#              ylim=(0, 8), yticks=np.arange(1, 8))
+#
+# plt.show()
