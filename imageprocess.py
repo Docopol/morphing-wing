1903037a0f9d6324c29072818e85ba85fffbdb37
JoshuaBog09 <95229035+JoshuaBog09@users.noreply.github.com>
2022-03-18 12:58:52 +0100

    grid creation and cropper

--- imageprocess.py
+++ imageprocess.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy
 
 from PIL import Image
 
@@ -44,20 +45,26 @@ def cropimage(array_color: np.ndarray, array_bool: np.ndarray) -> list:
 
     def ankerpoint2():
         for y in range(size[0] - 1, 3*(size[0]//5), -1):
-            if np.any(array_bool[y: 250:size[1]//2]):
+            if np.any(array_bool[y, 250:size[1]//2]):
                 break
         for x in range(0, size[1]//2):
             if np.any(array_bool[3*(size[0]//5):size[0],x]):
                 break
-        return y, x
+        for y2 in range(0, 2*(size[0]//5)):
+            if np.any(array_bool[y2,:]):
+                break
+        for x2 in range(size[1] - 1, 2*(size[1]//5), -1):
+            if np.any(array_bool[2*(size[0]//5):size[0],x2]):
+                break
+        return y, x, y2, x2
 
     size = np.shape(array_bool)
-    ankers = ankerpoint()
-    out_array_color = array_color[0:ankers[0] + 1, ankers[1]:size[1]]
-    out_array_bool = array_bool[0:ankers[0] + 1, ankers[1]:size[1]]
+    ankers = ankerpoint2()
+    out_array_color = array_color[ankers[2]-1:ankers[0]-1 , ankers[1]:ankers[3]+1]
+    out_array_bool = array_bool[ankers[2]-1:ankers[0]-1, ankers[1]:ankers[3]+1]
     return [out_array_color, out_array_bool]
 
-
+@perftimer
 def creategrid(array_color: np.ndarray, array_bool: np.ndarray, subgrid_size: int = 12, usedict:bool = False):
 
     def pad_with(vector, pad_width, iaxis, kwargs):
@@ -110,18 +117,23 @@ a = np.array([[False, False, False, False, False, False, False, False, False, Fa
      [False, False, False, False, False, False, False, True, False, False, False, False],
      [False, False, False, False, False, False, False, False, False, False, False, False]
      ])
-imageVisualization(a)
-imageVisualization(cropimage(a, a)[1])
-creategrid(np.array([[1,2],[2,3]]), np.array([[1,2],[2,3]]))
-creategrid(a,a, subgrid_size=4)
-
-
-images = files_in_directory("Images (BMP) - FOR IMAGE CALIBRATION ONLY", "bmp")
-image = loadRGBandBOOL(images[-1])
-
-imageVisualization(image[0])
-imageVisualization(image[1])
+# imageVisualization(a)
+# imageVisualization(cropimage(a, a)[1])
+# creategrid(np.array([[1,2],[2,3]]), np.array([[1,2],[2,3]]))
+# creategrid(a,a, subgrid_size=4)
+#
+#
+# images = files_in_directory("Images (BMP) - FOR IMAGE CALIBRATION ONLY", "bmp")
+# image = loadRGBandBOOL(images[-1])
+#
+# imageVisualization(image[0])
+# imageVisualization(image[1])
 
 img_bool_loc = files_in_directory("csv_bool", "csv")
 img_bool_file = load_file(img_bool_loc[1], separator=",", skip_last=True)
-imageVisualization(np.array(np.asarray(img_bool_file), dtype=bool)[2500:3000,750:1000])
+imageVisualization(img_bool_file)
+img_bool_cropped = cropimage(np.array(np.asarray(img_bool_file), dtype=bool), np.array(np.asarray(img_bool_file), dtype=bool))[1]
+imageVisualization(img_bool_cropped)
+points = np.argwhere(img_bool_cropped == True)
+plt.scatter(*zip(*points))
+plt.show()
