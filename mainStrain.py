c1ae470503dbe1211ff6417317233f5e630e7e80
Mick929 <mick.resink@gmail.com>
2022-03-23 10:43:41 +0100

    update
    update

--- mainStrain.py
+++ mainStrain.py
@@ -6,7 +6,7 @@ import processingFEMData
 import mainFEMdisp
 
 '''
-processedFEMData1s and processedFEMData2s are numpy arrays with [loadstepnumber - 1] [nodenumber - 11651,  strain xx]
+processedFEMData1s and processedFEMData2s are 3dnumpy arrays with [loadstepnumber - 1] [nodenumber - 11651,  strain xx]
 e.g. processedFEMData1s [2][:,1] -> gives the strain data from data set 1 for the 3rd loadstep 
 - Ask Mick if confused
 '''
@@ -14,7 +14,7 @@ processedFEMData1s = processingFEMData.processedFEMData1s
 processedFEMData2s = processingFEMData.processedFEMData2s
 
 '''
-femNodeLocations is a 2d array with [loadstepnumber -1] [summation of node positions distances]
+femNodeLocations is a 2d numpy array with [loadstepnumber -1] [summation of node positions distances]
 - Ask Mick if confused
 '''
 
@@ -22,8 +22,12 @@ femNodeLocations = np.array([mainFEMdisp.loadstep1_disp_loc[:-1], mainFEMdisp.lo
                             mainFEMdisp.loadstep3_disp_loc[:-1], mainFEMdisp.loadstep4_disp_loc[:-1],
                             mainFEMdisp.loadstep5_disp_loc[:-1]])
 
+'''
+is a 3d numpy array with [timeStampNumber] [length along contour (0) or strain (1)] [Loopletter (A,B,C,D)]
+note that A and B 
+'''
 
-
+timeStamps = [1, 13, 28, 40, 55]
 '''
 v v make graphs v v
 '''
@@ -37,7 +41,7 @@ for i in range (5):
     ax[0,0].plot(femNodeLocations[i], processedFEMData1s [i][:,1] * 10 ** 6, label = 'FEM', color = 'Blue')
     
     ax[0,0].set_ylabel('Microstrain [μm/m]')
-    ax[0,0].set_xlabel('Length along airfoil [m]')
+    ax[0,0].set_xlabel('Length along contour [m]')
     plt.legend()
 
 
@@ -46,82 +50,29 @@ for i in range (5):
     ax[0,1].set_title('Outside Experimental vs FEM strain plots over the length airfoil')
     #ax[0,1].plot(x1, y1, linewidth=2.0, label = 'Experimental', color = 'orange') 
     ax[0,1].plot(femNodeLocations[i], processedFEMData2s [i][:,1]* 10 ** 6, label = 'FEM', color = 'Blue')
+    
+    ax[0,1].set_ylabel('Microstrain [μm/m]')
+    ax[0,1].set_xlabel('Length along contour [m]')
     plt.legend()
     
 
     #symmetry analysis experiment (bending analysis)
+
+    plt.xticks(ticks = [-0.2,0.2], labels=['label1','label2'])
+    ax[1,0].set_ylabel('Microstrain [μm/m]')
+    ax[1,0].set_xlabel('Length along contour [m]')
 
     fig.delaxes(ax[1,1])
     
 plt.show()
 
 #asymmetry over time
+#plt.plot ()
 
-
-#synmmetry, time, 
-
-# symmetry (bending analysis), FEm vs experimental, time vs asymetry (optional)
-# fig, ax = plt.subplots(2,2)
-#
-# # symmetry analyis plot FEM
-# ax[0,0].plot(x1, y1, linewidth=2.0, label = 'sin', color = 'orange')
-#
-# ax[0,0].set(xlim=(0, 8), xticks=np.arange(1, 8),
-#        ylim=(0, 8), yticks=np.arange(1, 8))
-
-# ax[0,0].set_title('Bending Analyis FEM')
-
-# symmetry analyis plot FEM
-# ax[0,1].plot(x1, y1, linewidth=2.0, label = 'sin', color = 'orange')
-#
-# ax[0,1].set(xlim=(0, 8), xticks=np.arange(1, 8),
-#        ylim=(0, 8), yticks=np.arange(1, 8))
-#
-# ax[0,1].set_title('Bending Analyis FEM')
-'''
-# strain data 1
-plt.subplot(2, 2, 1)
-plt.plot(femNodeLocation[1 - 1], femStrainData1s[1 - 1][:, 4], label="loadstep 1.1")
-plt.plot(femNodeLocation[2 - 1], femStrainData1s[2 - 1][:, 4], label="loadstep 2.1")
-plt.plot(femNodeLocation[3 - 1], femStrainData1s[3 - 1][:, 4], label="loadstep 3.1")
-plt.plot(femNodeLocation[4 - 1], femStrainData1s[4 - 1][:, 4], label="loadstep 4.1")
-plt.plot(femNodeLocation[5 - 1], femStrainData1s[5 - 1][:, 4], label="loadstep 5.1")
-plt.legend()
-
-# strain data 2
-plt.subplot(2, 2, 2)
-plt.plot(femNodeLocation[1 - 1], femStrainData2s[1 - 1][:, 4], label="loadstep 1.2")
-plt.plot(femNodeLocation[2 - 1], femStrainData2s[2 - 1][:, 4], label="loadstep 2.2")
-plt.plot(femNodeLocation[3 - 1], femStrainData2s[3 - 1][:, 4], label="loadstep 3.2")
-plt.plot(femNodeLocation[4 - 1], femStrainData2s[4 - 1][:, 4], label="loadstep 4.2")
-plt.plot(femNodeLocation[5 - 1], femStrainData2s[5 - 1][:, 4], label="loadstep 5.2")
-plt.legend()
-
-# strain data difference of data 1 and 2
-plt.subplot(2, 2, 3)
-plt.plot(femNodeLocation[1 - 1], abs(abs(femStrainData1s[1 - 1][:, 4]) - abs(femStrainData2s[1 - 1][:, 4])),
-         label="loadstep 1")
-plt.plot(femNodeLocation[2 - 1], abs(abs(femStrainData1s[2 - 1][:, 4]) - abs(femStrainData2s[2 - 1][:, 4])), label="loadstep 2")
-plt.plot(femNodeLocation[3 - 1], abs(abs(femStrainData1s[3 - 1][:, 4]) - abs(femStrainData2s[3 - 1][:, 4])), label="loadstep 3")
-plt.plot(femNodeLocation[4 - 1], abs(abs(femStrainData1s[4 - 1][:, 4]) - abs(femStrainData2s[4 - 1][:, 4])), label="loadstep 4")
-plt.plot(femNodeLocation[5 - 1], abs(abs(femStrainData1s[5 - 1][:, 4]) - abs(femStrainData2s[5 - 1][:, 4])), label="loadstep 5")
-plt.legend()
+    
 plt.show()
 
-# # FEM vs experimental data
-# ax[1, 0].plot(x1, y1, linewidth=2.0, label='sin', color='orange')
-# ax[1, 0].plot(x1, y2, linewidth=2.0, label='cos', color='blue')
-#
-# ax[1, 0].set(xlim=(0, 8), xticks=np.arange(1, 8),
-#              ylim=(0, 8), yticks=np.arange(1, 8))
-#
-# ax[1, 0].legend(['sin', 'cos'])
-#
-# # asymmetry over time (optional)
-# ax[1, 1].plot(x1, y2, linewidth=2.0, label='cos', color='blue')
-#
+
+
 # ax[1, 1].set(xlim=(0, 8), xticks=np.arange(1, 8),
 #              ylim=(0, 8), yticks=np.arange(1, 8))
-#
-# plt.show()
-'''
