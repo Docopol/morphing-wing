6fe7a3d2d3b1f8d3f63ebf5181cdd0bb31e58e3e
JoshuaBog09 <95229035+JoshuaBog09@users.noreply.github.com>
2022-03-25 21:39:29 +0100

    working global variables

--- imageprocess.py
+++ imageprocess.py
@@ -10,6 +10,14 @@ from utils.filereader import files_in_directory
 from utils.filereader import load_file
 
 
+# global variables
+cut_left = 0
+cut_right = 0
+cut_up = 0
+cut_down = 0
+subgrid_size = 75
+
+
 @perftimer
 def imageVisualization(file, show_colorbar:bool = False, name:str = "") -> None:
     """
@@ -23,6 +31,12 @@ def imageVisualization(file, show_colorbar:bool = False, name:str = "") -> None:
 
 @perftimer
 def loadRGBandBOOL(file: str, usepil: bool = False) -> list:
+    """
+    :param file: The file path name from the repository root
+    :param usepil: True or False value depending on the wish to use the Pil libary or not
+    :return: A list containing the image array were every cell contains the rgb code of the pixel,
+    the second item is an initialized boolean array.
+    """
     if usepil:
         image_colordata = Image.open(file)
     else:
@@ -31,8 +45,7 @@ def loadRGBandBOOL(file: str, usepil: bool = False) -> list:
     return [np.asarray(image_colordata), file_bool]
 
 
-@perftimer
-def genBoolXY(array: np.ndarray) -> list:
+def genboolxy(array: np.ndarray) -> list:
 
     points = np.argwhere(array == True)
     x = []
@@ -45,10 +58,9 @@ def genBoolXY(array: np.ndarray) -> list:
     return [x, y]
 
 
-@perftimer
 def plotBoolpoints(array: np.ndarray, line: list = None) -> None:
 
-    points_x, points_y = genBoolXY(array)
+    points_x, points_y = genboolxy(array)
     plt.scatter(points_x, points_y)
     if line:
         x = line[0]
@@ -61,8 +73,12 @@ def plotBoolpoints(array: np.ndarray, line: list = None) -> None:
     plt.show()
 
 
-@perftimer
 def rotate(array: np.ndarray, amount: int = 1) -> np.ndarray:
+    """
+    :param array: A np.ndarray that is to be rotated
+    :param amount: Amount of times the array should be flipped 90degrees (were + is CW)
+    :return: Rotated array
+    """
     return np.rot90(array, k=amount)
 
 
@@ -72,11 +88,11 @@ def cropimage(array_color: np.ndarray, array_bool: np.ndarray) -> list:
     cropped the image based on an anker point found at the bottom left part of the screen
     """
 
-    def ankerpoint():
-        for y in range(size[0] - 1, 3*(size[0]//5), -1):
-            for x in range(0, size[1]//2):
-                if array_bool[y,x]:
-                    return y, x
+    # initialize global variables
+    global cut_left
+    global cut_right
+    global cut_up
+    global cut_down
 
     def ankerpoint2():
         for y in range(size[0] - 1, 3*(size[0]//5), -1):
@@ -95,13 +111,32 @@ def cropimage(array_color: np.ndarray, array_bool: np.ndarray) -> list:
 
     size = np.shape(array_bool)
     ankers = ankerpoint2()
-    out_array_color = array_color[ankers[2]:ankers[0]-1 , ankers[1]:ankers[3]+1]
+
+    print(size)
+    print(ankers)
+
+    cut_left = ankers[1] - 1
+    cut_right = size[1] - ankers[3]
+    cut_up = ankers[2]
+    cut_down = size[0] - ankers[0]
+
+    print(f"{cut_left=}")
+    print(f"{cut_right=}")
+    print(f"{cut_up=}")
+    print(f"{cut_down=}")
+
+    out_array_color = array_color[ankers[2]:ankers[0]-1, ankers[1]:ankers[3]+1]
     out_array_bool = array_bool[ankers[2]:ankers[0]-1, ankers[1]:ankers[3]+1]
+
+    print(f"{np.shape(out_array_bool)=}")
     return [out_array_color, out_array_bool]
 
 
 @perftimer
-def creategrid(array_bool: np.ndarray, subgrid_size: int = 12, usedict:bool = False):
+def creategrid(array_bool: np.ndarray, usedict:bool = False):
+
+    # initialize global variable
+    global subgrid_size
 
     def pad_with(vector, pad_width, iaxis, kwargs):
         pad_value = kwargs.get('padder', 0)
@@ -139,9 +174,27 @@ def creategrid(array_bool: np.ndarray, subgrid_size: int = 12, usedict:bool = Fa
 
 
 @perftimer
-def regress(array: np.ndarray, subgrid_size: int = 12):
+def regress(array: np.ndarray):
 
-    def setupLS(array: np.ndarray, index: tuple = (0, 0), subgrid_size: int = 12) -> list:
+    # initialize global variable
+    global subgrid_size
+
+    def genBoolXY(array: np.ndarray) -> list:
+
+        points = np.argwhere(array == True)
+        x = []
+        y = []
+        for i in points:
+            x.append(i[0])
+            y.append(i[1])
+        x = np.array(x)
+        y = np.array(y)
+        return [x, y]
+
+    def setupLS(array: np.ndarray, index: tuple = (0, 0)) -> list:
+
+        # initialize global variable
+        global subgrid_size
 
         x, y = genBoolXY(array)
 
@@ -167,19 +220,29 @@ def regress(array: np.ndarray, subgrid_size: int = 12):
 
     weight = np.zeros(size)
     slope = np.zeros(size)
-    centroid = np.zeros(size)
+    centroidX = np.zeros(size)
+    centroidY = np.zeros(size)
+
 
     for i in range(int(size[0])):
         for j in range(int(size[1])):
             loc_array = array[i,j]
             if np.count_nonzero(loc_array == True):
+                # some debugging code
                 # if ok == 5 or ok == 10:
                     # imageVisualization(loc_array)
                     # plotBoolpoints(rotate(loc_array, amount=-1), line=setupLS(rotate(loc_array, amount=-1), index=(i,j), subgrid_size= subgrid_size))
                     # setupLS(rotate(loc_array, amount=-1), index=(i,j), subgrid_size= subgrid_size)
                     # plt.show()
+                res = setupLS(rotate(loc_array, amount=0), index=(i, j))
 
-                setupLS(rotate(loc_array, amount=0), index=(i, j), subgrid_size=subgrid_size)
+                # data of all the subgrids
+                weight[i, j] = np.count_nonzero(loc_array) / subgrid_size
+                slope[i, j] = res[1]
+                centroidX[i, j] = res[3]
+                centroidY[i, j] = res[4]
+
+    centroid = np.dstack((centroidX, centroidY))
     plt.show()
 
 
@@ -211,7 +274,6 @@ a = np.array([[False, False, False, False, False, False, False, False, False, Fa
 # imageVisualization(image[0])
 # imageVisualization(image[1])
 
-SG = 25
 
 img_bool_loc = files_in_directory("csv_bool", "csv")
 img_bool_file = load_file(img_bool_loc[2], separator=",", skip_last=True)
@@ -219,8 +281,8 @@ img_bool_file = load_file(img_bool_loc[2], separator=",", skip_last=True)
 img_bool_cropped = cropimage(np.array(np.asarray(img_bool_file), dtype=bool), np.array(np.asarray(img_bool_file), dtype=bool))[1]
 imageVisualization(img_bool_cropped)
 #plotBool(rotate(img_bool_cropped,3))
-img_grid = creategrid(img_bool_cropped, subgrid_size= SG)
-regress(img_grid, SG)
+img_grid = creategrid(img_bool_cropped)
+regress(img_grid)
 
 
 # img_bool_file2 = load_file(img_bool_loc[0], separator=",", skip_last=True)
@@ -234,3 +296,12 @@ regress(img_grid, SG)
 # print(*zip(*points))
 # plt.scatter(*zip(*points))
 # plt.show()
+
+
+# sometesting = load_file(img_bool_loc[3], separator=",", skip_last= False)
+# sometesting = np.asarray(sometesting)
+# for level in range(np.shape(sometesting)[0]):
+#
+#     plt.scatter(sometesting[:,2], sometesting[:,1])
+# plt.show()
+
