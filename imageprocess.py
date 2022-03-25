cacdab8d235be08aee32b99c6ac7c313e56e0606
JoshuaBog09 <95229035+JoshuaBog09@users.noreply.github.com>
2022-03-25 22:30:37 +0100

    addition of documentation

--- imageprocess.py
+++ imageprocess.py
@@ -32,19 +32,24 @@ def imageVisualization(file, show_colorbar:bool = False, name:str = "") -> None:
 @perftimer
 def loadRGBandBOOL(file: str, usepil: bool = False) -> list:
     """
-    :param file: The file path name from the repository root
+    :param file: The file path name from the repository root of the image
     :param usepil: True or False value depending on the wish to use the Pil libary or not
     :return: A list containing the image array were every cell contains the rgb code of the pixel,
-    the second item is an initialized boolean array.
+    the second item is an initialized boolean array with the size of the color array.
     """
+
+    # generate file with methode of choice
     if usepil:
         image_colordata = Image.open(file)
     else:
         image_colordata = plt.imread(file)
+
+    # intialize empty boolean array with the size of the color array
     file_bool = np.asarray(np.zeros((np.copy(image_colordata).shape[:2]), dtype=bool))
     return [np.asarray(image_colordata), file_bool]
 
 
+# Outdated function used for debugging
 def genboolxy(array: np.ndarray) -> list:
 
     points = np.argwhere(array == True)
@@ -57,7 +62,7 @@ def genboolxy(array: np.ndarray) -> list:
     y = np.array(y)
     return [x, y]
 
-
+# Outdated function used for debugging
 def plotBoolpoints(array: np.ndarray, line: list = None) -> None:
 
     points_x, points_y = genboolxy(array)
@@ -85,7 +90,12 @@ def rotate(array: np.ndarray, amount: int = 1) -> np.ndarray:
 @perftimer
 def cropimage(array_color: np.ndarray, array_bool: np.ndarray) -> list:
     """
-    cropped the image based on an anker point found at the bottom left part of the screen
+    :param array_color: Color image array to be cropped
+    :param array_bool: Boolean array to be cropped
+    :return: cropped image(color and bool in a list) based on two anker point,
+     found at the bottom left and top right part of the screen.
+     The ankerpoints are based on locations were no critical data will be left
+     after linear cropping.
     """
 
     # initialize global variables
@@ -94,7 +104,10 @@ def cropimage(array_color: np.ndarray, array_bool: np.ndarray) -> list:
     global cut_up
     global cut_down
 
-    def ankerpoint2():
+    def ankerpoint2() -> tuple:
+        """
+        :return: Two anker points in a list/ tuple
+        """
         for y in range(size[0] - 1, 3*(size[0]//5), -1):
             if np.any(array_bool[y, 250:size[1]//2]):
                 break
@@ -115,6 +128,7 @@ def cropimage(array_color: np.ndarray, array_bool: np.ndarray) -> list:
     print(size)
     print(ankers)
 
+    # save the amount that is going to be cropped on all sides
     cut_left = ankers[1] - 1
     cut_right = size[1] - ankers[3]
     cut_up = ankers[2]
@@ -125,6 +139,7 @@ def cropimage(array_color: np.ndarray, array_bool: np.ndarray) -> list:
     print(f"{cut_up=}")
     print(f"{cut_down=}")
 
+    # crop both the color and boolean array
     out_array_color = array_color[ankers[2]:ankers[0]-1, ankers[1]:ankers[3]+1]
     out_array_bool = array_bool[ankers[2]:ankers[0]-1, ankers[1]:ankers[3]+1]
 
@@ -133,31 +148,48 @@ def cropimage(array_color: np.ndarray, array_bool: np.ndarray) -> list:
 
 
 @perftimer
-def creategrid(array_bool: np.ndarray, usedict:bool = False):
+def creategrid(array_bool: np.ndarray, usedict:bool = False) -> np.ndarray:
+    """
+    :param array_bool: Cropped or non-cropped boolean array, to be devided into subgrids
+    which are later to be used for linear/ or other global contour approximation.
+    :param usedict: For debugging perpose, dump the array data as a dictionary.
+    :return: A 4 dimensional array containing subgrids(of requested size) on each of the mxn elements.
+    """
 
     # initialize global variable
     global subgrid_size
 
     def pad_with(vector, pad_width, iaxis, kwargs):
+        """
+        :param vector: 2 dimension array to be padded
+        :param pad_width: Direction to be padded by requested amount
+        :param iaxis: /
+        :param kwargs: padder = some value <- the value wich is used to pad the array (0, False in this use case)
+        :return: A padded array in the requested direction
+        """
         pad_value = kwargs.get('padder', 0)
         vector[:pad_width[0]] = pad_value
         if pad_width[1] != 0:
             vector[-pad_width[1]:] = pad_value
 
+    # Find the amount an array should be padded to allow for grid creation based on the requested subgrid size
     size = np.shape(array_bool)
 
     size_corectionY = subgrid_size - size[0] % subgrid_size
     size_corectionX = subgrid_size - size[1] % subgrid_size
 
+    # Pad the array in vertical and or horizontal direction to allow for grid creation
     if size_corectionY != subgrid_size:
         array_bool = np.pad(array_bool, ((size_corectionY, 0), (0, 0)), pad_with, padder=0)
     if size_corectionX != subgrid_size:
         array_bool = np.pad(array_bool, ((0, 0), (0, size_corectionX)), pad_with, padder=0)
 
+    # Initialize dump and subgrid array
     size = np.shape(array_bool)
     subgrids_dict = {}
     subgrids = []
 
+    # Create and store the subgrids
     for i in range(0, size[0], subgrid_size):
         empty_1 = []
         for j in range(0, size[1], subgrid_size):
@@ -174,12 +206,24 @@ def creategrid(array_bool: np.ndarray, usedict:bool = False):
 
 
 @perftimer
-def regress(array: np.ndarray):
+def regress(array: np.ndarray) -> None:
+    """
+    :param array: 4 dimensional array containing subgrids,
+    using the subgrids the contour of the airfoil can be approximated using a linear approximation.
+    linear approximation used: Least squares
+    :return: Pyplot containing the approximated airfoil contour
+    """
 
     # initialize global variable
     global subgrid_size
 
     def genBoolXY(array: np.ndarray) -> list:
+        """
+        :param array: 2 dimensional boolean array (subgrid).
+        :return: A list containing the location of the True values of the specified input array.
+        The coordinates are defined from the top left, the x(vertical) and y(horizontal)
+        are defined to plot the aifoil on its side.
+        """
 
         points = np.argwhere(array == True)
         x = []
@@ -192,48 +236,62 @@ def regress(array: np.ndarray):
         return [x, y]
 
     def setupLS(array: np.ndarray, index: tuple = (0, 0)) -> list:
+        """
+        :param array: The boolean array(subgrid) of the airfoil LE contour on wich least squares should be performed.
+        :param index: current index of subgrid to translate to the correct global location.
+        :return: list with unique x values, slope and intercept found from least square,average x and y.
+        A global plot of the approximated airfoil LE contour is also contructed in the meantime.
+        """
 
         # initialize global variable
         global subgrid_size
 
+        # get x and y coordinates of True points in array/subgrid
         x, y = genBoolXY(array)
 
+        # Transform x and y to global position
         x += index[0]*subgrid_size
         y += index[1]*subgrid_size
 
-        # plt.scatter(x, y)
+        ##  plt.scatter(x, y) # plot contour points (DEBUG)
 
+        # centroid of array/ subgrid of bool values where True has weight 1 and False 0
         x_avg = np.mean(x)
         y_avg = np.mean(y)
 
+        # Least squares
         A = np.vstack([x, np.ones(len(x))]).T
         a, b = np.linalg.lstsq(A, y, rcond=None)[0]
 
         x = np.array(list(set(x)))
 
         plt.plot(x, a * x + b, "black")
-        #plt.plot(x_avg, y_avg, 'ro')
+
+        ##  plt.plot(x_avg, y_avg, 'ro') # plot centroid points (DEBUG)
 
         return [x, a, b, x_avg, y_avg]
 
     size = np.shape(array)[0:2]
 
+    # Initialize arrays containing subgrid values(Weight(amount of True over total items in array/subgrid),
+    # slope, centroid) for the ful global contour
     weight = np.zeros(size)
     slope = np.zeros(size)
     centroidX = np.zeros(size)
     centroidY = np.zeros(size)
 
-
+    # perform least squares for every subgrid in main global array
     for i in range(int(size[0])):
         for j in range(int(size[1])):
             loc_array = array[i,j]
             if np.count_nonzero(loc_array == True):
-                # some debugging code
-                # if ok == 5 or ok == 10:
+                ## some debugging code (DEBUG)
+                ## if ok == 5 or ok == 10:
                     # imageVisualization(loc_array)
                     # plotBoolpoints(rotate(loc_array, amount=-1), line=setupLS(rotate(loc_array, amount=-1), index=(i,j), subgrid_size= subgrid_size))
                     # setupLS(rotate(loc_array, amount=-1), index=(i,j), subgrid_size= subgrid_size)
                     # plt.show()
+                # Least square
                 res = setupLS(rotate(loc_array, amount=0), index=(i, j))
 
                 # data of all the subgrids
@@ -245,7 +303,7 @@ def regress(array: np.ndarray):
     centroid = np.dstack((centroidX, centroidY))
     plt.show()
 
-
+# Test array
 a = np.array([[False, False, False, False, False, False, False, False, False, False, False, False],
      [False, False, False, False, False, False, False, False, False, False, False, True],
      [False, False, False, False, False, False, False, False, False, False, False, False],
@@ -262,6 +320,11 @@ a = np.array([[False, False, False, False, False, False, False, False, False, Fa
      [False, False, False, False, False, False, False, True, False, False, False, False],
      [False, False, False, False, False, False, False, False, False, False, False, False]
      ])
+
+# -----------------
+# Main calling code
+# -----------------
+
 # imageVisualization(a)
 # imageVisualization(cropimage(a, a)[1])
 # creategrid(np.array([[1,2],[2,3]]), np.array([[1,2],[2,3]]))
--- utils/filereader.py
+++ utils/filereader.py
@@ -3,6 +3,13 @@ import glob
 
 
 def load_file(file: str, separator="\t", skip_last: bool = None, skip_rows: int = None):
+    """
+    :param file: File path relative to repository root
+    :param separator: Used seperator standard \t
+    :param skip_last: Skip the last column (Bool)
+    :param skip_rows: Skip specified amount of rows
+    :return: pandas data frame
+    """
     data_file = pd.read_csv(file, sep=separator, skiprows=skip_rows)
     headers = list(data_file.columns.values)
 
@@ -16,4 +23,9 @@ def load_file(file: str, separator="\t", skip_last: bool = None, skip_rows: int 
 
 
 def files_in_directory(folder: str, file_extension: str) -> list:
+    """
+    :param folder: Folder directory relative to repository root.
+    :param file_extension: Fine extension (e.g. .txt).
+    :return: File paths of all files(realtive to repo root) in the requested folder.
+    """
     return glob.glob(f"Data/{folder}/*.{file_extension}")
