bd656e5caf302efdf339579a97f5f0baba98e573
Roel <r.vanleeuwen-4@student.tudelft.nl>
2022-04-18 18:16:05 +0200

    update actuationForce

--- actuationForce.py
+++ actuationForce.py
@@ -8,6 +8,7 @@ def f3(x: np.ndarray):
     y = (x - 0.3) / ((1.75 - 0.3) / (2500 - 0))
     return y
 
+
 # def f5(x):
 #     y = (x - 0.4) / ((3.9 - 0.4) / (1800 - 0))
 #     return y
@@ -37,10 +38,11 @@ calibration_data = [readFile("Data/DispAndForce/CurrentSensorCalibration3.csv"),
 # ____Calculate the displacement of the actuator in mm
 actuator_disp = []
 for index_dis in range(len(data_real[:])):
-    actuator_disp.append(10*data_real[index_dis]["Voltage power source [V]"][:] / 2)
-
+    actuator_disp.append(10 * data_real[index_dis]["Voltage power source [V]"][:] / 2)
 
 # ____Calculate the current with the real power source voltages and the calibrationVoltageReadings3
+
+
 current_real = []
 
 current_real_interpolation = scipy.interpolate.interp1d(calibration_data[1]["Voltage power source [V]"][:],
@@ -59,10 +61,13 @@ for index_dis_csv in range(len(data_real[:])):
 
     all_data_i = np.zeros((len(data_real[index_dis_csv]["Time [s]"][:]), 7))
     for index_total in range(len(data_real[index_dis_csv]["Time [s]"][:])):
-        all_data_i[index_total, 0] = data_real[index_dis_csv]["Time [s]"][index_total]-data_real[index_dis_csv]["Time [s]"][0]
+        all_data_i[index_total, 0] = data_real[index_dis_csv]["Time [s]"][index_total] - \
+                                     data_real[index_dis_csv]["Time [s]"][0]
         all_data_i[index_total, 1] = actuator_disp[index_dis_csv][index_total]
         all_data_i[index_total, 2] = f3(current_real[index_dis_csv][index_total, 1])
         all_data_i[index_total, 3] = current_real[index_dis_csv][index_total, 1]
+        all_data_i[index_total, 4] = data_real[index_dis_csv]["Voltage power source [V]"][index_total]
+        all_data_i[index_total, 5] = data_real[index_dis_csv]["Voltage current sensor [V]"][index_total]
         # all_data_i[index_total, 3] = f5(current_real[index_dis_csv][index_total, 1])
         # all_data_i[index_total, 4] = f6(current_real[index_dis_csv][index_total, 1])
         # all_data_i[index_total, 5] = f9(current_real[index_dis_csv][index_total, 1])
@@ -70,21 +75,40 @@ for index_dis_csv in range(len(data_real[:])):
     all_data.append(all_data_i)
 
 
+
+# find relation between current sensor voltage and current
+
+# interpolate so that power source voltage = f(current sensor voltage)
+interp_list_voltages_real = []
+for disp_index in range(4):
+    psv_real_interp = scipy.interpolate.interp1d(all_data[disp_index][:, 5], all_data[disp_index][:, 4],
+                                                 fill_value='extrapolate')
+    interp_list_voltages_real.append(psv_real_interp)
+
+# interpolate so that power source voltage = f(current sensor voltage) but now for SensorCalibration3
+interp_list_voltages_cal = []
+for disp_index in range(4):
+    psv_real_interp_cal = scipy.interpolate.interp1d(calibration_data[:, 2], calibration_data[:, 1],
+                                                 fill_value='extrapolate')
+    interp_list_voltages_cal.append(psv_real_interp_cal)
+
+
+
 # print(max(all_data[2][:, 2]))
 # print(all_data[2][:, 1])
 
 # ____put plots below____
 
-for index3 in range(4):
-    plt.xlabel("Time [s]")
-    plt.ylabel("Displacement [mm]")
-    label = ["Target: 10 mm", "Target: 20 mm", "Target: 30 mm", "Target: 39.2 mm"]
-    plt.plot(all_data[index3][:, 0], all_data[index3][:, 1], label = label[index3])
-    plt.legend()
+# for index3 in range(4):
+#     plt.xlabel("Time [s]")
+#     plt.ylabel("Displacement [mm]")
+#     label = ["Target: 10 mm", "Target: 20 mm", "Target: 30 mm", "Target: 39.2 mm"]
+#     plt.plot(all_data[index3][:, 0], all_data[index3][:, 1], label = label[index3])
+#     plt.legend()
+#
+# plt.show()
 
-plt.show()
-#
-#
+
 # plt.xlabel("Time [s]")
 # plt.ylabel("Voltage power source [V]")
 # plt.plot(calibration_data[0]["Time [s]"][:], calibration_data[0]["Voltage power source [V]"][:])
@@ -101,14 +125,21 @@ plt.show()
 # plt.show()
 
 
-plt.xlabel("Time [s]")
-plt.ylabel("Force [N]")
+# plt.xlabel("Time [s]")
+# plt.ylabel("Force [N]")
 
-for index5 in range(4):
-    plt.plot(all_data[index5][:, 0], all_data[index5][:, 2])
+# for index5 in range(4):
+#     plt.plot(all_data[index5][:, 0], all_data[index5][:, 2])
+#
+# plt.scatter([3.6, 6.0, 8.3, 10.4], calibration_data[2]["Actuation force [N]"][:], label="FEM data")
+# plt.legend()
+# plt.show()
 
-plt.scatter([3.6, 6.0, 8.3, 10.4], calibration_data[2]["Actuation force [N]"][:], label="FEM data")
-plt.legend()
+for index3 in range(4):
+    plt.xlabel("Time [s]")
+    plt.ylabel("Voltage current sensor [V]")
+    label = ["Target: 10 mm", "Target: 20 mm", "Target: 30 mm", "Target: 39.2 mm"]
+    plt.plot(all_data[index3][:, 0], all_data[index3][:, 5], label=label[index3])
+    plt.legend()
+
 plt.show()
-
-
